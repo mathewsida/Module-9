88acee4fc426e262995480cb9a3c209712b57dc3
Mathew Sida-Nguon <sida.mathew@gmail.com>
2023-01-19 23:12:32 -0800

    committing

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,111 @@
-// TODO: Include packages needed for this application
+// variables needed to run node and use the input.
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// links generateMarkdown file to connect them together.
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Questions to prompt the user 
+const questions = 
 
-// TODO: Create a function to initialize app
-function init() {}
+[{
+    type: 'input',
+    message: 'What is the title of the project?',
+    name: 'Title'
+},
 
-// Function call to initialize app
+{ 
+ type:'input',
+ message:'what is the project about? Give a detailed description of your project.',
+ name:'Description'
+},
+
+
+{
+    type:'input',
+    message:'What needs to be installed to run the app?',
+    name:'Installation'
+},
+
+
+
+{
+    type:'input',
+    message:'How is the application used?',
+    name:'Usage'
+},
+
+
+{
+    type:'input',
+    message:'Contact info for inquiries.',
+    name:'Contact'
+},
+
+
+{
+    type:'input',
+    message:'What is your Github username?',
+    name:'Github'
+},
+
+
+{
+    type:'input',
+    message:'What is your github Repo name?',
+    name:'Repo'
+},
+
+
+{
+    type:'list',
+    message:'What license did you use?',
+    name:'License',
+
+    choices: ['MIT', 'GPU', 'ISC', 'Apache']
+},
+
+
+{
+
+    type:'input',
+    message:'What is your email address?',
+    name:'Email'
+},
+
+
+{
+    type:'input',
+    message:'Who was involved in creating this application?',
+    name:'Credits'
+},
+
+
+{
+    type:'input',
+    message:'How did you test your application',
+    name:'Tests'
+}];
+
+
+
+// function to create and write the file after running through node.
+function writeToFile(fileName, data) { 
+  fs.writeFile(fileName, data, (err) => {
+    err ? console.log(err) : console.log('Congratulations on creating your own README file.')
+  })
+    
+}
+
+// function to initialize program and generate the readme file.
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+       const generated = generateMarkdown(data)
+       writeToFile('./destination/README.md', generated)
+    })
+ }
+
+
 init();
